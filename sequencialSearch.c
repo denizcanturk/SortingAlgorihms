2a69cecfbb74d9b5d6c2d5b8a65af3a2a8c0177a
Deniz CANTURK <40653685+denizcanturk@users.noreply.github.com>
2020-05-30 15:38:07 +0300

    Create sequencialSearch.c

---
+++ sequencialSearch.c
@@ -0,0 +1,33 @@
+/*
+#include <stdio.h>
+
+/* Ardışık arama motoru
+ * Yazan Tosun
+ * Bunu bilgisayarda yazdırana kosun...
+ * Sevgilerle....*/
+
+
+
+void ardisik_arama(int arr[], int aranan){
+    int sizeofit = sizeof (arr)/ sizeof (arr [0]);
+
+    int i = 0;
+    for (i = 0; i < sizeofit ; i++){
+        if (arr[i]== aranan){
+            printf("%d sayisi, dizinin %d konumunda ...\n ", arr[i], i+1);
+            break;
+        }
+
+    }
+     if (i == sizeofit)
+         printf("%d sayısı dizi de bulunamadı\n", aranan);
+}
+
+int main(){
+   int aranan1 = 11, aranan2 = 8;
+   int arr[] = {10,14,11,226,13};
+   ardisik_arama(arr,aranan1);
+   ardisik_arama(arr,aranan2);
+   printf("Please press any key to continue...");
+   getchar();
+}
